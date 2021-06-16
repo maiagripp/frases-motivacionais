7b99041026c8151405cfd4d0393044a6729d517b
maiagripp <maiaandradec@gmail.com>
2021-06-16 11:42:31 -0300

    Implementando UseState

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
 import FraseMotivacional from './components/FraseMotivacional';
 import './App.css';
+import { useState } from 'react';
+import Formulario from './components/Formulario';
 
 const App = () => {
-  const frases = [
-    {
-      texto: 'A vingança nunca é plena, mata a alma e envenena',
-      autor: 'Seu Madruga',
-      criadaEm: new Date(2021, 5, 15),
-    },
-    {
-      texto: 'Camarão que dorme a onda leva',
-      autor: 'Zeca Pagodinho',
-      criadaEm: new Date(2021, 5, 14),
-    },
-    {
-      texto:
-        '- Doi né? - O que? - deitar no sofá e lembrar que esqueceu o controle.',
-      autor: 'Autor desconhecido',
-      criadaEm: new Date(2021, 5, 13),
-    },
-  ];
+  const [frases, setFrases] = useState([]);
+
+  const adicionaFrase = (frase) => {
+    frase.criadaEm = new Date();
+    setFrases([frase, ...frases]);
+  };
+
   return (
     <div className="container">
       <h1>Frases Motivacionais para alegrar seu dia</h1>
-      <FraseMotivacional
-        frase={frases[0].texto}
-        autor={frases[0].autor}
-        criadaEm={frases[0].criadaEm}
-      />
-      <FraseMotivacional
-        frase={frases[1].texto}
-        autor={frases[1].autor}
-        criadaEm={frases[1].criadaEm}
-      />
-      <FraseMotivacional
-        frase={frases[2].texto}
-        autor={frases[2].autor}
-        criadaEm={frases[2].criadaEm}
-      />
+
+      <Formulario aoSalvar={adicionaFrase} />
+
+      {frases.map((frase, indice) => (
+        <FraseMotivacional
+          key={indice}
+          frase={frase.texto}
+          autor={frase.autor}
+          criadaEm={frase.criadaEm}
+        />
+      ))}
     </div>
   );
 };
---
+++ src/components/Formulario/index.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './style.css';
+
+const Formulario = ({ aoSalvar }) => {
+  const [autor, setAutor] = useState('');
+  const [texto, setTexto] = useState('');
+
+  const manipuladorAutor = (evento) => {
+    if (evento.target.value.split(' ').length <= 2) {
+      setAutor(evento.target.value);
+    }
+  };
+  const manipuladorTexto = (evento) => {
+    setTexto(evento.target.value);
+  };
+  const salvar = (evento) => {
+    evento.preventDefault();
+    const frase = {};
+    frase.autor = autor;
+    frase.texto = texto;
+    aoSalvar(frase);
+    setAutor('');
+    setTexto('');
+  };
+
+  return (
+    <form onSubmit={salvar}>
+      <div className="formulario">
+        <label>Autor</label>
+        <input value={autor} onChange={manipuladorAutor} />
+      </div>
+      <div className="formulario">
+        <label>Texto</label>
+        <textarea value={texto} onChange={manipuladorTexto}></textarea>
+      </div>
+      <button>Salvar</button>
+    </form>
+  );
+};
+
+export default Formulario;
